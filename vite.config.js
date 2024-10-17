dceaf622d214f8dc82b380313b8ff2cd899f0f26
hlii0279 <hlii0279@student.monash.edu>
2024-10-17 18:56:49 +1100

    change vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,5 @@
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -29,6 +11,21 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: (import.meta.env.MODE === 'production' ? '/hli-library/' : '/') || '/'
+  }
 })
+
+// import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   base: (import.meta.env.MODE === 'production' ? '/hli-library/' : '/') || '/'
+// })
